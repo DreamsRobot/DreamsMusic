699f31c4b8cfb1c3b0d00c1e4cdd5ca3d433e132
DreamsRobot <hdhindihub@gmail.com>
2025-08-04 15:38:43 +0530

    Update stats.py

--- DreamsMusic/handlers/stats.py
+++ DreamsMusic/handlers/stats.py
@@ -1,1 +1,8 @@
-# Placeholder for stats command
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from DreamsMusic.utils.stats import get_stats
+
+@Client.on_message(filters.command("stats"))
+async def bot_stats(client, message: Message):
+    stats = get_stats()
+    await message.reply(stats)
