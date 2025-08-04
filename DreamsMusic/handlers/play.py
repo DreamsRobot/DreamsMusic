bb382832864ae842790869d8cb710814c0bca466
DreamsRobot <hdhindihub@gmail.com>
2025-08-04 15:38:04 +0530

    Update play.py

--- DreamsMusic/handlers/play.py
+++ DreamsMusic/handlers/play.py
@@ -1,5 +1,13 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
+from DreamsMusic.core.player import MusicPlayer
+
+player = MusicPlayer()
 
 @Client.on_message(filters.command("play"))
-async def play_handler(client, message):
-    await message.reply_text("Playing requested song...")
+async def play_command(client, message: Message):
+    query = " ".join(message.command[1:])
+    if not query:
+        return await message.reply("Usage: /play [song name or YouTube link]")
+    status = await player.play(query, message.chat.id)
+    await message.reply(status)
