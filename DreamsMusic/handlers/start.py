04fab94898f689841a9a26eb510c066c85f61a84
DreamsRobot <hdhindihub@gmail.com>
2025-08-04 15:38:25 +0530

    Update start.py

--- DreamsMusic/handlers/start.py
+++ DreamsMusic/handlers/start.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 
 @Client.on_message(filters.command("start"))
-async def start_handler(client, message):
-    await message.reply_text("DreamsMusic is alive! Send /help to see commands.")
+async def start_bot(client, message: Message):
+    await message.reply("👋 Hello! I'm DreamsMusic bot.\nType /help for commands.")
